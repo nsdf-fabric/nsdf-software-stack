cd6dede0b13f61ecd8b838801c584aa808364cc8
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-09-20 22:32:38 -0400

    Updating cyverse

--- nsdf/catalog/cyverse.py
+++ nsdf/catalog/cyverse.py
@@ -3,9 +3,7 @@ import os,sys,requests,time, datetime,urllib3, requests,json,copy, psutil, json,
 import pprint
 from pprint import pprint, pformat
 
-
 sys.path.append(".")
-from nsdf.kernel import WorkerPool
 
 num_cached_response=0
 num_network_response=0
@@ -24,40 +22,27 @@ def GetNetworkResponse(url, headers={}, params={},connection_timeout=10,response
 
 
 # ///////////////////////////////////////////////////////////////////////////////////
-def GetCachedResponse(url, headers=None, params=None):
+def GetCachedResponse(cache_filename, url, headers=None, params=None):
 
 	global num_cached_response,num_network_response
-
-	CACHE_DIR=os.environ["CACHE_DIR"]
-	os.makedirs(CACHE_DIR,exist_ok=True)
-
-	# I don't want the token to be considered for caching
-	headers_copy=copy.deepcopy(headers)
-	headers_copy.pop("Authorization")
-
-	import hashlib
-	m5 = hashlib.md5(json.dumps({
-	     "url":url, 
-	     "headers":headers_copy, 
-	     "params": params
-     }).encode()).hexdigest()
-
-	cache_filename=os.path.join(CACHE_DIR,f"{m5}.json")
+ 
 	if os.path.isfile(cache_filename):
 		# print("Reading",cache_filename)
 		with open(cache_filename,"r") as fin:
 			num_cached_response+=1
 			return json.loads(fin.read())
 	else:
-		num_network_response+=1
+		os.makedirs(os.path.dirname(cache_filename),exist_ok=True)
 		try:
 			response=GetNetworkResponse(url, headers=headers, params=params)
 		except Exception as ex:
+			print(ex)
 			return ex
-		os.makedirs(os.path.dirname(cache_filename),exist_ok=True)
+		
 		with open(cache_filename,"w") as fout:
 			fout.write(json.dumps(response))
-		# print("Wrote",cache_filename,"for",url)
+		print("Wrote",cache_filename,"for",url)
+		num_network_response+=1
 		return response
 
 
@@ -71,10 +56,11 @@ def cint(value):
 # ///////////////////////////////////////////////////////////////////////////////////
 def Main():
 
+	# https://de.cyverse.org/data/ds/iplant/home/shared/NEON_workshop/data
 	ROOT_FOLDER='/iplant/home/shared'
 	csv_filename='/srv/nvme1/nsdf/cyverse.csv'
  
-	os.environ["CACHE_DIR"]="/srv/nvme0/nsdf/cyverse-cache"
+	
 	
 	# https://de.cyverse.org/terrain/docs/index.html
 	"""
@@ -90,69 +76,72 @@ def Main():
 	username = os.environ["CYVERSE_USERNAME"]
 	password = os.environ["CYVERSE_PASSWORD"]
 	CYVERSE_TOKEN = requests.get("https://de.cyverse.org/terrain/token", auth=(username, password)).json()['access_token']
+	print("Got CYVERSE_TOKEN",CYVERSE_TOKEN)
 
 	io1 = psutil.net_io_counters()
 	T1=time.time()
  
-	# https://datadryad.org/api/v2/docs/
-	# curl -X GET "https://datadryad.org/api/v2/datasets?page=0&per_page=100" -H "accept: application/json"
+
+	NUM_FILES,TOT_SIZE=0,0
 
 	t1=time.time()
-	def print_stats(force=False):
+	def print_stats(current_folder=None, force=False):
 		nonlocal t1
 		if force or (time.time()-t1)>2.0:
 			t1=time.time()
 			io2 = psutil.net_io_counters()
-			print(f"tot_size={tot_size:,} tot_size-tb={tot_size/1024**4:.2f} num_files={num_files:,} num_cached_response={num_cached_response:,} num_network_response={num_network_response:,} num-network-upload-bytes={io2.bytes_sent - io1.bytes_sent:,} network-download-bytes={io2.bytes_recv - io1.bytes_recv:,} sec={time.time()-T1:.2f}")
+			print(f"current_folder={current_folder} tot_size={TOT_SIZE:,} tot_size-tb={TOT_SIZE/1024**4:.2f} num_files={NUM_FILES:,} num_cached_response={num_cached_response:,} num_network_response={num_network_response:,} num-network-upload-bytes={io2.bytes_sent - io1.bytes_sent:,} network-download-bytes={io2.bytes_recv - io1.bytes_recv:,} sec={time.time()-T1:.2f}")
 
-	def MyTask(pool, folder):
 
-		folder_body = GetCachedResponse("https://de.cyverse.org/terrain/filesystem/paged-directory", 
+	def Traverse(csv_writer, folder,rec):
+   
+		nonlocal NUM_FILES,TOT_SIZE
+		CACHE_DIR="/srv/nvme0/nsdf/cyverse-cache"
+  
+		folder_body = GetCachedResponse(
+    								f"{CACHE_DIR}{folder}.json",
+            				"https://de.cyverse.org/terrain/filesystem/paged-directory", 
 		                 headers={
                    		"Authorization": "Bearer " + CYVERSE_TOKEN, 
                      	'Accept': '`application/json'
                      },
 		                 params={
-                   		'path':folder, 
+                   		'path': folder, 
                      	'entity-type':'any',  
-                      'limit':'9999999999999999'
+                      'limit':'9999999999999999',
+                      'sort-col':'NAME',
+                      'sort-dir':'ASC'
                      })
 
 		if isinstance(folder_body,Exception): 
 			print(f"Folder={folder} returned error {folder_body}")
 			return
-
-		# recursive
-		sub_folders=[it for it in folder_body.get('folders',[]) if 'path' in it]
-		for sub in sub_folders:
-			pool.pushTask(functools.partial(MyTask,pool, sub['path']))
  
-		files=[file for file in folder_body.get('files',[]) if 'path' in file and 'file-size' in file]
-
+		# files
 		tot_size=0
+		files=[file for file in folder_body.get('files',[]) if 'path' in file and 'file-size' in file]
 		for file in files:
 			# catalog,bucket,name,size,last_modified,etag	
 			size=cint(file['file-size'])
 			row=["cyverse",os.path.dirname(file['path']),os.path.basename(file['path']),size,file.get('date-modified',''),'']
 			tot_size+=size
-			pool.results.put(row)
-		
-		print(f"Done folder={folder} num-folders={len(sub_folders)} num-files={len(files):,} tot-size={tot_size:,}")
-		print_stats()
-	
-	pool=WorkerPool()
-	pool.setMaxConcurrency(4)
-	pool.pushTask(functools.partial(MyTask,pool, ROOT_FOLDER))
+			csv_writer.writerow(row)
+			TOT_SIZE+=size
+			NUM_FILES+=1   
+			print_stats(folder)
+ 
+		# recursive
+		sub_folders=[it for it in folder_body.get('folders',[]) if 'path' in it]
+		for sub in sub_folders:
+			Traverse(csv_writer, sub['path'],rec+1)
+			print_stats(folder)  
 
+		# print(f"Done folder={folder} num-folders={len(sub_folders)} num-files={len(files):,} tot-size={tot_size:,}"
+	
 	with open(csv_filename, 'w') as fout:
 		csv_writer = csv.writer(fout)
-		num_files,tot_size=0,0
-		for row in pool:
-			csv_writer.writerow(row)
-			tot_size+=row[3]
-			num_files+=1   
-			print_stats()
-		print_stats(force=True)
+		Traverse(csv_writer, ROOT_FOLDER,0)
+		print_stats(None,force=True)
  
 # ///////////////////////////////////////////////////////////////////////////////////
 if __name__=="__main__":
